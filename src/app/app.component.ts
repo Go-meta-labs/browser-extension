2db20546045f54e963aff2556faae24dd58cc7e9
gometalabs <gometalaunch@aol.com>
2022-04-06 10:35:50 +0100

    chore: generate UI page components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'extension';
+  color: string = '#ffffff';
+
+  ngOnInit(): void {
+    chrome.storage.sync.get('color', ({ color }) => {
+      this.color = color;
+    });
+  }
+
+  public updateColor() {
+    console.log(this.color)
+    chrome.storage.sync.set({color: this.color});
+    this.colorize()
+  }
+
+  public colorize() {
+      chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        chrome.tabs.executeScript(
+          tabs[0].id!,
+          { code: `document.body.style.backgroundColor = '${ this.color }';` }
+        );
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './pages/home/home.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { TransactionsComponent } from './pages/transactions/transactions.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    HomeComponent,
+    DashboardComponent,
+    PortfolioComponent,
+    TransactionsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
